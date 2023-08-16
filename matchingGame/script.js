77161f8dc3590edb112fd42a4b4a1aa85d16cb28
DukeZhu95 <128326731+DukeZhu95@users.noreply.github.com>
2023-08-16 12:42:08 +1200

    Update script.js

--- matchingGame/script.js
+++ matchingGame/script.js
@@ -1,18 +1,19 @@
 $(document).ready(function() {
     const images = [
-        'images/icons8-butterfly-filled-100.png',
-        'images/icons8-chicken-filled-100.png',
-        'images/icons8-crab-filled-100.png',
-        'images/icons8-dog-filled-100.png',
-        'images/icons8-dolphin-filled-100.png',
-        'images/icons8-dove-filled-100.png',
-        'images/icons8-elephant-filled-100.png',
-        'images/icons8-horse-filled-100.png'
+        '../images/icons8-butterfly-filled-100.png',
+        '../images/icons8-chicken-filled-100.png',
+        '../images/icons8-crab-filled-100.png',
+        '../images/icons8-dog-filled-100.png',
+        '../images/icons8-dolphin-filled-100.png',
+        '../images/icons8-dove-filled-100.png',
+        '../images/icons8-elephant-filled-100.png',
+        '../images/icons8-horse-filled-100.png'
     ];
 
-    let cards = [...images, ...images]; // Duplicate the array for pairs
+    // 创建一个包含所有图片两次的数组
+    const allImages = images.concat(images);
 
-    // Fisher-Yates Shuffle
+    // 洗牌函数
     function shuffle(array) {
         let currentIndex = array.length, randomIndex;
 
@@ -20,50 +21,37 @@ $(document).ready(function() {
             randomIndex = Math.floor(Math.random() * currentIndex);
             currentIndex--;
 
-            [array[currentIndex], array[randomIndex]] = [
-                array[randomIndex], array[currentIndex]
-            ];
+            [array[currentIndex], array[randomIndex]] = [array[randomIndex], array[currentIndex]];
         }
 
         return array;
     }
 
-    shuffle(cards);
-
-    for(let i = 0; i < 4; i++) {
-        let row = $('<tr></tr>');
-        for(let j = 0; j < 4; j++) {
-            let cell = $('<td></td>');
-            let card = $('<img>')
-                .attr('src', cards[i*4 + j])
-                .addClass('card');
-            cell.append(card);
-            row.append(cell);
-        }
-        $('#game table').append(row);
-    }
-
     let flippedCards = [];
 
-    $('img.card').on('click', function() {
-        if (flippedCards.length < 2 && !$(this).hasClass('flipped')) {
-            $(this).addClass('flipped');
-            flippedCards.push(this);
+    $('td').on('click', function() {
+        const cardImage = $(this).find('img.card');
+        if (flippedCards.length < 2 && !cardImage.hasClass('flipped')) {
+            cardImage.show().addClass('flipped');
+            flippedCards.push(cardImage);
+        }
+
+        if (flippedCards.length === 2) {
+            setTimeout(function() {
+                if (flippedCards[0].attr('src') !== flippedCards[1].attr('src')) {
+                    flippedCards[0].hide().removeClass('flipped');
+                    flippedCards[1].hide().removeClass('flipped');
+                }
+                flippedCards = [];
+            }, 1000); // 1秒后检查是否匹配
         }
     });
 
-    $('#continue').on('click', function() {
-        if (flippedCards.length === 2) {
-            if (flippedCards[0].src === flippedCards[1].src) {
-                // Cards match
-                $(flippedCards[0]).off('click').hide();
-                $(flippedCards[1]).off('click').hide();
-            } else {
-                // Turn them face down
-                $(flippedCards[0]).removeClass('flipped');
-                $(flippedCards[1]).removeClass('flipped');
-            }
-            flippedCards = [];
-        }
+    // 洗牌图片
+    const shuffledImages = shuffle(allImages);
+
+    // 将图片添加到表格中
+    $('td').each(function(index) {
+        $(this).html('<img src="../images/' + shuffledImages[index] + '" alt="animal image">');
     });
 });
